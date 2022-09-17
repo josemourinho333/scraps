f3d0340ca722c0208f412bcb0044c8dae1630755
root <root@PhilYoo.localdomain>
2022-09-17 12:04:44 -0700

    Add pre-existing entry check before scraping and adding to db;

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,6 @@ const axios = require('axios');
 const cors = require('cors');
 const db = require('./database/database');
 const listingRoute = require('./routes/listings');
-const { stringify } = require('querystring');
 
 // port and express init
 const port = process.env.PORT || 8080;
@@ -44,54 +43,77 @@ axios.get(process.env.CRAIGSLIST_URL)
       return unwanted;
     })
     .then((unwanted) => {
-      let limit = 3;
-      $('.result-row', html).each(function(index) {
-        if (index < limit) {
+      let limit = 5;
+      db.query(`SELECT craigslist_id FROM listings;`).then(({ rows: results}) => {
+        const listingIds = results.map((result) => Number(result.craigslist_id));
+        console.log('listingids', listingIds);
+        $('.result-row', html).each(function(index) {
+          if (index < limit) {
+            const currentElement = $(this);
+            const craigslistId = Number(currentElement.attr('data-pid'));
 
-
-          const currentElement = $(this);
-          const title = currentElement.find('.result-title').text().toLowerCase();
-          const condition = (element) => title.includes(element);
+            if (!listingIds.includes(craigslistId)) {
+              const title = currentElement.find('.result-title').text().toLowerCase();
+              const condition = (element) => title.includes(element);
+              
+              // need to make db query to grab unwanted stuff to use below
+              if (!unwanted.years.some(condition) && !unwanted.keywords.some(condition)) {
+                const price = Number(currentElement.children('.result-info').find('.result-price').text().slice(1).split('').filter((char) => char !== ',').join(''));
+                const images = !currentElement.find('.result-image').attr('data-ids')
+                  ? []
+                  : currentElement.find('.result-image').attr('data-ids').split(',').map((ids) => {
+                    return `https://images.craigslist.org/${ids.slice(2)}_300x300.jpg`;
+                });
+                const date = currentElement.children('.result-info').find('.result-date').attr('datetime');
+                const location = currentElement.children('.result-info').find('.result-hood').text();
+                const link = currentElement.children('.result-image').attr('href');
           
-          // need to make db query to grab unwanted stuff to use below
-          if (!unwanted.years.some(condition) && !unwanted.keywords.some(condition)) {
-            const id = Number(currentElement.attr('data-pid'));
-            const price = Number(currentElement.children('.result-info').find('.result-price').text().slice(1));
-            const images = !currentElement.find('.result-image').attr('data-ids')
-            ? []
-            : currentElement.find('.result-image').attr('data-ids').split(',').map((ids) => {
-              return `https://images.craigslist.org/${ids.slice(2)}_300x300.jpg`;
-            })
-          ;
-            const date = currentElement.children('.result-info').find('.result-date').attr('datetime');
-            const location = currentElement.children('.result-info').find('.result-hood').text();
-            const link = currentElement.children('.result-image').attr('href');
-            
-            // check the ids against ones in DB and filter those out before acutally making the calls
-            // make individual page fetch request timed, as to not get blocked
-            // build listing object and "INSERT INTO listings..."
-            setTimeout(() => {
-              axios.get(link).then((res) => {
-                const html = res.data;
-                const $ = cheerio.load(html);
-                console.log('id', id);
-                console.log('price', price);
-                console.log('date', date);
-                console.log('images', images);
-                console.log('location', location);
-                console.log('post title', $('.body', html).find('.postingtitle').text());
-                console.log('desc', $('.body', html).find('#postingbody').not($('.body', html).find('#postingbody').children()).text().trim());
-                console.log('condition', $('.body', html).find('.attrgroup').children('span:first-child').children('b').text());
-              })
-            }, index * 1000);
+                // make individual page fetch request timed, as to not get blocked
+                setTimeout(() => {
+                  axios.get(link).then((res) => {
+                    const html = res.data;
+                    const $ = cheerio.load(html);
+                    const title = $('.body', html).find('.postingtitle').text().trim();
+                    const desc = $('.body', html).find('#postingbody').not($('.body', html).find('#postingbody').children()).text().trim();
+                    const condition = $('.body', html).find('.attrgroup').children('span:first-child').children('b').text();
+                    const item = {
+                      craigslistId,
+                      link,
+                      title,
+                      date,
+                      desc,
+                      price,
+                      condition,
+                      location,
+                      images
+                    };
+
+                    return item;
+                  })
+                  .then((item) => {
+                    // need to update table with make, model and size
+                    const vals = [item.craigslistId, item.link, item.title, item.desc, item.condition, item.price, item.date, item.location];
+                    db.query(`
+                      INSERT INTO listings 
+                        (craigslist_id, link, title, description, condition, price, date, location) 
+                      VALUES
+                        ($1, $2, $3, $4, $5, $6, $7, $8)
+                      RETURNING *;`
+                    , vals)
+                    .then((addedItem) => {
+                      console.log('item added', addedItem);
+                      console.log('images', item.images);
+                    })
+                  })
+                }, index * 1000);
+              }
+            }
           }
-
-
-        }
-      })
+        })
+      });
     })
     .catch((err) => console.log('err', err));
-
+     
     // let limit = 3;
     // $('.result-row', html).each(function(index) {
     //   if (index < limit) {
