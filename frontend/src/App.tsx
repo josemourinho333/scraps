8fda81d6f7b6a2aec840902769fe43182d137632
root <root@PhilYoo.localdomain>
2022-09-23 18:28:18 -0700

    Add function for updating excluding content

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -96,6 +96,10 @@ const App = () => {
       .catch((err) => console.log('err', err));
   };
 
+  const unwantedContent = () => {
+    console.log('post request sent for:')
+  };
+
   return (
     <BrowserRouter>
       <Nav />
